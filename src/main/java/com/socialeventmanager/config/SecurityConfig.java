21efb0c4bbd6be4e5b431f198da8d061db19da14
Facundo Torres <ftorres@ubicuagroup.com>
2026-05-02 15:36:36 -0300

    cors config

--- src/main/java/com/socialeventmanager/config/SecurityConfig.java
+++ src/main/java/com/socialeventmanager/config/SecurityConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,6 +30,7 @@ public class SecurityConfig {
             AuthenticationProvider authenticationProvider) throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
@@ -79,15 +78,4 @@ public class SecurityConfig {
             return config.getAuthenticationManager();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                    @Override
-                    public void addCorsMappings(CorsRegistry registry) {
-                            registry.addMapping("/**")
-                                            .allowedOrigins("*")
-                                            .allowedMethods("*");
-                    }
-            };
-    }
 }
